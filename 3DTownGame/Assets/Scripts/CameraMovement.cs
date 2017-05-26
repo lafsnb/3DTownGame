12a99f73217d7d93b5ba465e63af03ad0989f340
Isaac <ikingst7@gmail.com>
2017-05-25 19:19:30 -0600

    Created cameramovement script

---
+++ 3DTownGame/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+	public float cameraMovementSpeed = 2f;
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mouseOffset = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+		GetComponent<Camera>().transform.Rotate(mouseOffset, Space.Self);
+		//auto-up the lazy way:
+		GetComponent<Camera>().transform.LookAt(
+			GetComponent<Camera>().transform.position + GetComponent<Camera>().transform.forward * cameraMovementSpeed, 
+			Vector3.up);
+
+
+	}
+}
