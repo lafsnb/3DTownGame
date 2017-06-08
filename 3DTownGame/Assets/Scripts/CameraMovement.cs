e347c01cda4b0ac21c40376c1019cda29012e36a
Isaac <ikingst7@gmail.com>
2017-06-08 09:19:52 -0600

    Updated player movement/rotation
    fixed rotation bug, by removing box collider on player
    changed movement script

--- 3DTownGame/Assets/Scripts/CameraMovement.cs
+++ 3DTownGame/Assets/Scripts/CameraMovement.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour {
 
-	public float cameraMovementSpeed = 2f;
-	
-	// Update is called once per frame
 	void Update () {
 
-		Vector3 mouseOffset = new Vector3(-Input.GetAxis("Mouse Y") * cameraMovementSpeed, Input.GetAxis("Mouse X") * cameraMovementSpeed, 0f);
-		GetComponent<Camera>().transform.Rotate(mouseOffset, Space.Self);
-		//auto-up the lazy way:
-		GetComponent<Camera>().transform.LookAt(
-			GetComponent<Camera>().transform.position + GetComponent<Camera>().transform.forward, 
-			Vector3.up);
+		Vector3 mouseOffset = new Vector3(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0f);
+		transform.Rotate(mouseOffset, Space.Self);
 
+		transform.LookAt(transform.position + transform.forward, Vector3.up);
 
 	}
 }
